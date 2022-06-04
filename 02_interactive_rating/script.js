734d126f14e1cf02b218c9c39fac9cbd67c6aa4b
Justin Cho <jchoustin91@gmail.com>
2022-06-03 22:16:25 -0700

    Reset Rating Color

---
+++ 02_interactive_rating/script.js
@@ -0,0 +1,32 @@
+//Todo
+//User clicks on a rating
+//- save in variable
+//- take all chosen classes off the ratings (if there are any)
+//- add the chosen class to the correct button to change the color.
+//On Submit
+
+//Elements to get:
+// 1. Buttons
+// 2. Submit Btn
+
+let userRating = 0;
+const ratings = document.getElementsByClassName('btn rating-btn');
+
+console.log(ratings);
+
+document.addEventListener('click', e => {
+  e.preventDefault();
+  if (!e.target.classList.contains('rating-btn')) {
+    return;
+  }
+
+  //Update userRating
+  userRating = e.target.innerHTML;
+  //Reset Rating list
+  resetChosenRating();
+  e.target.classList.toggle('chosen-rating');
+});
+
+const resetChosenRating = () => {
+  console.log('reset');
+};
