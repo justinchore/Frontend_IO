f2f4701e6421ceb0f4435487b062110df88002ca
Justin Cho <jchoustin91@gmail.com>
2022-06-05 21:17:44 -0700

    Update rating element with submit

--- 02_interactive_rating/script.js
+++ 02_interactive_rating/script.js
@@ -3,7 +3,10 @@
 //- save in variable
 //- take all chosen classes off the ratings (if there are any)
 //- add the chosen class to the correct button to change the color.
+// - get the rating and insert into rating msg
 //On Submit
+// - have the rating card disappear ('hidden')
+// - Remove hidden from the thank you card
 
 //Elements to get:
 // 1. Buttons
@@ -11,6 +14,8 @@
 
 let userRating = 0;
 const ratings = [...document.getElementsByClassName('btn rating-btn')];
+const ratingElement = document.getElementById('rating');
+const ratingContainer = document.getElementsByClassName(rating - container);
 
 console.log(ratings);
 
@@ -27,6 +32,7 @@ document.addEventListener('click', e => {
     e.target.classList.toggle('chosen-rating');
   } else if (e.target.classList.contains('submit-btn')) {
     console.log('submit Button!');
+    ratingElement.innerHTML = userRating;
   }
 
   return;
